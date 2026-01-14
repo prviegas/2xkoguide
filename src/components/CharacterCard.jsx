00da15a941992381f2452839d9fb7797c4fc3725
prviegas <prvieg@gmail.com>
2026-01-13 21:34:12 -0300

    Update CharacterCard styles and structure for improved icon display

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -4,7 +4,9 @@ function CharacterCard({ character, onClick }) {
   return (
     <div className="character-card" onClick={() => onClick(character)}>
       <div className="character-icon">
-        <img src={`../../public/champion-pics/${character.name}1.png`} alt={character.name} />
+        <div>
+          <img src={`../../public/champion-pics/${character.name}1.png`} alt={character.name} />
+        </div>
       </div>
       <h3>{character.name}</h3>
     </div>
