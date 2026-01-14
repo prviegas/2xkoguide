39629324dc0358477a9a855eb74b14acc2269944
prviegas <prvieg@gmail.com>
2026-01-14 01:32:38 -0300

    Fixing image paths for deployment

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,11 +1,11 @@
 import './CharacterCard.css'
 
-function CharacterCard({ character, onClick }) {
+function CharacterCard({ character, onClick, image }) {
   return (
     <div className="character-card" onClick={() => onClick(character)}>
       <div className="character-icon">
         <div>
-          <img src={`../../public/champion-pics/${character.name}1.png`} alt={character.name} />
+          <img src={image} alt={character.name} />
         </div>
       </div>
       <h3>{character.name}</h3>
--- src/components/CharacterGrid.jsx
+++ src/components/CharacterGrid.jsx
@@ -1,7 +1,39 @@
 import CharacterCard from './CharacterCard'
 import './CharacterGrid.css'
+import ahriImage from '../../public/champion-pics/ahri1.png'
+import braumImage from '../../public/champion-pics/braum1.png'
+import dariusImage from '../../public/champion-pics/darius1.png'
+import ekkoImage from '../../public/champion-pics/ekko1.png'
+import illaoiImage from '../../public/champion-pics/illaoi1.png'
+import jinxImage from '../../public/champion-pics/jinx1.png'
+import viImage from '../../public/champion-pics/vi1.png'
+import yasuoImage from '../../public/champion-pics/yasuo1.png'
+
 
 function CharacterGrid({ characters, onCharacterClick }) {
+  const getCharacterImage = (name) => {
+    switch (name.toLowerCase()) {
+      case 'ahri':
+        return ahriImage
+      case 'braum':
+        return braumImage
+      case 'darius':
+        return dariusImage
+      case 'ekko':
+        return ekkoImage
+      case 'illaoi':
+        return illaoiImage
+      case 'jinx':
+        return jinxImage
+      case 'vi':
+        return viImage
+      case 'yasuo':
+        return yasuoImage
+      default:
+        return null
+    }
+  }
+
   return (
     <div className="characters-grid">
       {characters.map(character => (
@@ -9,6 +41,7 @@ function CharacterGrid({ characters, onCharacterClick }) {
           key={character.id}
           character={character}
           onClick={onCharacterClick}
+          image={getCharacterImage(character.name)}
         />
       ))}
     </div>
